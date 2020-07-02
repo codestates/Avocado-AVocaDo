1ba03ac36551cabe1a15ff2af7cd7de480579b22
lllgresi <61128600+lllgresi@users.noreply.github.com>
2020-07-02 14:38:55 +0900

    complete client routing #15
    react-router-dom, axios 설치했습니다.
    Main.js, Header.js, WordList.js 컴포넌트 생성했습니다.
    index.js, App.js, Main.js, Header.js에 라우팅 구현했습니다.
    Main.js에 API요청 함수 작성했습니다.

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom'; // userHistory
+
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Main from './components/Main';
+
+class App extends React.Component {
+  state = {
+    isLogin: false,
+    userInfo: null,
+  };
+
+  handleLogin() {
+    this.setState({
+      isLogin: true,
+    });
+  }
+  handleLogout() {
+    this.setState({
+      isLogin: false,
+    });
+  }
+
+  // getUserData()
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path="/" render={() => <Login />} />
+          <Route exact path="/signup" render={() => <SignUp />} />
+          <Route
+            exact
+            path="/main"
+            render={() => (
+              <Main />
+              // <WordInput />
+              // <WordCard />
+              // <WordCardStack />
+            )}
+          />
+          <Route
+            path="/"
+            render={() => {
+              if (this.state.isLogin) {
+                return <Redirect to="/main" />;
+              }
+              return <Redirect to="/" />;
+            }}
+          />
+        </Switch>
+      </div>
+    );
+  }
+}
+export default App;
+
+// #15 클라이언트 라우팅 설계
+// react-router-dom 설치했습니다.
+// Main.js, Header.js, WordList.js 컴포넌트 생성했습니다.
+// index.js, App.js, Main.js, Header.js에 라우팅 구현했습니다.
