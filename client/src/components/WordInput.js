09acc89c93a1a685572205bf09f99ad00ad26355
lllgresi <61128600+lllgresi@users.noreply.github.com>
2020-07-03 11:41:44 +0900

    add handleWordInput function in wordinput.js

--- client/src/components/WordInput.js
+++ client/src/components/WordInput.js
@@ -5,12 +5,14 @@ class WordInput extends Component {
   constructor(props) {
     super(props);
 
-    //   this.handleWordInput = this.handleWordInput.bind(this);
-    // }
-
-    // handleWordInput = (e) => {
-    //   this.setState({ currentWord: e.target.value })
+    this.handleWordInput = this.handleWordInput.bind(this);
   }
+
+  handleWordInput = (e) => {
+    this.setState({ currentWord: e.target.value });
+    // 이 컴포넌트에 state가 없는데 setState를 쓸 수 있는지?
+    // 확인 필요
+  };
 
   render() {
     const { postInputWord } = this.props;
@@ -21,14 +23,17 @@ class WordInput extends Component {
             className="wordinput_form"
             onSubmit={(e) => {
               e.preventDefault();
-              // this.handleWordInput('currentWord');
               postInputWord();
             }}
           >
             <div className="wordinput_area">
               <div className="wordinput_row">
                 <span className="input_box">
-                  <input type="text" placeholder="단어를 입력하세요"></input>
+                  <input
+                    type="text"
+                    placeholder="단어를 입력하세요"
+                    onChange={this.handleWordInput}
+                  ></input>
                   <input type="submit" value="🥑" />
                 </span>
               </div>
