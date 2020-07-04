449359f4bac28d809ce01a250bd2fc4a4d3ee2a8
gyeongwon1275 <rlaruddnjs28@gmail.com>
2020-07-04 12:45:12 +0900

    Add finding word overlap when add word #22
    kmp 알고리즘 모듈을 사용하여 단어추가시 중복되는 단어일때 alert 창
    띄우게함

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import Header from './Header';
 import WordInput from './WordInput';
 import WordCardStack from './WordCardStack';
+// npm i --save kmp-matcher
+import kmp from 'kmp-matcher';
 import '../CSS/Main.css';
 
 class Main extends React.Component {
@@ -67,10 +69,66 @@ class Main extends React.Component {
   };
 
   addWordData = () => {
-    this.state.wordData.push({ word: this.state.currentWord, sentences: [] });
-    this.setState({ wordData: this.state.wordData });
+    // 단어만 추가할 때는 sentences 는 빈배열
+    // 모달창에서 예문추가하면 sentences 에 엔터단위로 split 에서 push 함
+    //
+
+    // 단어가 중복이면 메서드가 실행되서는 안됨
+    // 이미 단어가 있습니다!! => alert
+    // kmp.kmp(전체문자열, 찾고자 하는 특정 문자열)
+
+    // kmp.kmp() -> 길이가 1보다 크면 alert 띄우고 아니면 push 한다.
+
+    // 전체문자열 -> 전체 단어의 모음
+    // 특정문자열 -> this.state.currentWord
+    // const wordArr = [];
+    // this.state.wordData.forEach(element => {
+
+    //   wordArr.push(element.word)
+    // });
+
+    // const stringTotalWord = JSON.stringify(wordArr);
+    // "word":"원하는단어"
+    // findingWord =
+    const findingWord = `"word":"${this.state.currentWord}"`;
+    console.log(
+      '확인',
+      kmp.kmp(JSON.stringify(this.state.wordData), findingWord)
+    );
+
+    const searchResult =
+      kmp.kmp(JSON.stringify(this.state.wordData), findingWord).length > 0;
+
+    if (searchResult) {
+      alert('이미 단어장에 있는 단어입니다!');
+    } else {
+      this.state.wordData.push({ word: this.state.currentWord, sentences: [] });
+      this.setState({ wordData: this.state.wordData });
+    }
   };
 
+  handleSentenseData = (sentences) => {
+    const splitSentences = sentences.split('\n');
+    // 단어가 중복되어 들어가게 된다.
+    // 똑같은 단어가 있으면 해당 단어data 를 삭제하고 덮어씌워야
+    // push 로 하면 안되!
+    // setState 로 변경해야 함
+    // 현재단어에 있는 문장에만 push 해야 함
+
+    // 현재단어를 특정해야 함
+
+    this.state.wordData.push({
+      word: this.state.currentWord,
+      sentences: splitSentences,
+    });
+    this.setState({ wordData: this.state.wordData });
+
+    console.log('handleSentenseData', this.state);
+  };
+
+  componentDidMount() {
+    // index 를 리턴한다.
+  }
   render() {
     console.log('render', this.state);
     return (
@@ -99,6 +157,9 @@ class Main extends React.Component {
             postInputWord={this.postInputWord.bind(this)}
             updateWordData={this.updateWordData.bind(this)}
             deleteWordData={this.deleteWordData.bind(this)}
+            addWordData={this.addWordData.bind(this)}
+            handleInput={this.handleInput.bind(this)}
+            handleSentenseData={this.handleSentenseData.bind(this)}
           />
         </div>
       </div>
@@ -106,17 +167,17 @@ class Main extends React.Component {
   }
 }
 
-Main.propTypes = {
-  isLogin: PropTypes.bool.isRequired,
-  currentWord: PropTypes.string.isRequired,
-  wordData: PropTypes.object.isRequired,
-  userInfo: PropTypes.object.isRequired,
-  handleInput: PropTypes.func.isRequired,
-  handleLogout: PropTypes.func.isRequired,
-  postInputWord: PropTypes.func.isRequired,
-  addWordData: PropTypes.func.isRequired,
-  updateWordData: PropTypes.func.isRequired,
-  deleteWordData: PropTypes.func.isRequired,
-};
+// Main.propTypes = {
+//   isLogin: PropTypes.bool.isRequired,
+//   currentWord: PropTypes.string.isRequired,
+//   wordData: PropTypes.object.isRequired,
+//   userInfo: PropTypes.object.isRequired,
+//   handleInput: PropTypes.func.isRequired,
+//   handleLogout: PropTypes.func.isRequired,
+//   postInputWord: PropTypes.func.isRequired,
+//   addWordData: PropTypes.func.isRequired,
+//   updateWordData: PropTypes.func.isRequired,
+//   deleteWordData: PropTypes.func.isRequired,
+// };
 
 export default Main;
