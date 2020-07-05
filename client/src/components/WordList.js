3e68426cc6ea42f105cd9ce32e68672b0b99869c
lllgresi <61128600+lllgresi@users.noreply.github.com>
2020-07-05 16:58:02 +0900

    make wordList functional component

--- client/src/components/WordList.js
+++ client/src/components/WordList.js
@@ -3,43 +3,36 @@ import WordCard from './WordCard';
 import PropTypes from 'prop-types';
 import '../CSS/Wordbook.css';
 
-class WordList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const {
-      wordData,
-      postInputWord,
-      updateWordData,
-      deleteWordData,
-      handleSentenceData,
-    } = this.props;
-
-    return (
-      <div className="wordlist_wrap">
-        <div className="wordlist_stack">
-          {wordData
-            ? wordData.map((word, index) => {
-                return (
-                  <WordCard
-                    key={index}
-                    word={word.word}
-                    sentences={word.sentences}
-                    index={index}
-                    postInputWord={postInputWord}
-                    updateWordData={updateWordData}
-                    deleteWordData={deleteWordData}
-                    handleSentenceData={handleSentenceData}
-                  />
-                );
-              })
-            : 'noWord!'}
-        </div>
+// pagination을 위해 함수 컴포넌트로 변경
+function WordList({
+  wordData,
+  postInputWord,
+  updateWordData,
+  deleteWordData,
+  handleSentenceData,
+}) {
+  return (
+    <div className="wordlist_wrap">
+      <div className="wordlist_stack">
+        {wordData
+          ? wordData.map((word, index) => {
+              return (
+                <WordCard
+                  key={index}
+                  word={word.word}
+                  sentences={word.sentences}
+                  index={index}
+                  postInputWord={postInputWord}
+                  updateWordData={updateWordData}
+                  deleteWordData={deleteWordData}
+                  handleSentenceData={handleSentenceData}
+                />
+              );
+            })
+          : 'noWord!'}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 WordList.propTypes = {
