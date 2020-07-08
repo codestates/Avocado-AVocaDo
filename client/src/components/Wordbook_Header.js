90eb1a2623a95ab8b8329f597849475a143fbd79
gyeongwon1275 <rlaruddnjs28@gmail.com>
2020-07-08 16:00:39 +0900

    add function searching word  #12
    단어만 검색할 수 있는 searchbar

--- client/src/components/Wordbook_Header.js
+++ client/src/components/Wordbook_Header.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { Form, Button } from 'react-bootstrap';
 import '../CSS/Wordbook.css';
+import { func } from 'prop-types';
 
 
-function Wordbook_Header() {
+function Wordbook_Header(props) {
+
+  const { searchText, isSearch, handleSearch } = props;
+  const [searchInput, setSearchText] = useState('');
+
+
+  function handleInputSearch(e) {
+
+    setSearchText(e.target.value)
+
+    console.log('searchText', searchInput)
+  }
+
+  function querySearchText() {
+
+    const queryText = document.querySelector('.form-control').value;
+    handleSearch(queryText);
+  }
   return (
     <nav className="wordbook_nav">
       <Link to="/main">
         <h3>이전 페이지로</h3>
       </Link>
+
+      <div className="form_search_box">
+        <Form onSubmit={querySearchText} >
+          <Form.Control
+            inline
+            type="text"
+            placeholder="검색"
+            maxLength="20"
+            onChange={handleInputSearch}
+          />
+        </Form>
+        <button type="submit" title="검색" className="btn_submit"
+        onClick={querySearchText}
+        >
+          <span className="ico_search_submit">
+          </span>
+        </button>
+      </div>
       <Link to="/">
         <h3>LOGOUT</h3>
       </Link>
