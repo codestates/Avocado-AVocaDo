71218b6ff4266a8d8c2261917bfacd73b8dbf4a4
pingu9 <h1h2h1@naver.com>
2020-07-10 10:38:06 +0900

    페북 파일 수정

--- server/controller/users/signin.js
+++ server/controller/users/signin.js
@@ -5,7 +5,8 @@ const request = require('request');
 const { User } = require('../../models');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../../../.env') });
-const access_token = process.env.FACEBOOK_ACCESS_TOKEN;
+// const access_token = process.env.FACEBOOK_ACCESS_TOKEN;
+const access_token = '1217568225253856|FoxJJZdueieUJtKvnDsVbQw6rYY';
 const crypto = require('crypto');
 
 module.exports = {
